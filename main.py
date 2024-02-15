9fc95046f2449c09bb7eb0a4f4292b51a427cc35
Solractys <135873875+Solractys@users.noreply.github.com>
2024-02-15 19:54:08 -0300

    again

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
+from tkinter import scrolledtext
 import cv2
-import pytesseract
+import time
 
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+COLORS = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
+
+class_names = []
+with open("coco.names", "r") as f:
+    class_names = [cname.strip() for cname in f.readlines()]
 
 cap = cv2.VideoCapture(0)
 
+net = cv2.dnn.readNet("yolov4-tiny.weights", "yolov4.cfg")
+
+model = cv2.dnn_DetectionModel(net)
+model.setInputParams(size=(416, 416), scale=1/255)
+
 while True:
-    ret, frame = cap.read()
+    _, frame = cap.read()
+    start = time.time()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    classes, scores, boxes = model.detect(frame, 0.2, 0.6)
+    
+    end = time.time()
 
-    text = pytesseract.image_to_string(gray)
+    for (classid, score, box) in zip(classes, scores, boxes):
+        color = COLORS[int(classid) % len(COLORS)]
+        label = f"{class_names[classid]} : {score}"
+        cv2.rectangle(frame, box, color, 2)
+        cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_DUPLEX, 0.5, color, 2)
 
-    cv2.imshow('Webcam', frame)
-    print('Texto reconhecido:', text)
+    fps_label = f"FPS: {round(1.0/(end - start), 2)}"
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 5)
+    cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0), 3)
+
+    cv2.imshow("detections", frame)
+    if cv2.waitKey(1) == 27:
         break
-
 cap.release()
 cv2.destroyAllWindows()
